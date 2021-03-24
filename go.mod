ab0da7d25340d20cd4516ca8de60c81cbccca165
Jason Zhang <jasonzhang9309@163.com>
2021-03-24 17:44:24 +0800

    add sfs_turbo resource

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/hashicorp/terraform-plugin-sdk v1.16.0
-	github.com/huaweicloud/golangsdk v0.0.0-20210318062012-78ebc939936a
-	github.com/huaweicloud/terraform-provider-huaweicloud v1.22.3-0.20210322091131-919d73033fb6
+	github.com/huaweicloud/golangsdk v0.0.0-20210323020602-6d5ee0030244
+	github.com/huaweicloud/terraform-provider-huaweicloud v1.22.3-0.20210325093634-f44e24d59cc2
 )
---
+++ sbercloud/resource_sbercloud_sfs_turbo_test.go
@@ -0,0 +1,159 @@
+package sbercloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/huaweicloud/golangsdk/openstack/sfs_turbo/v1/shares"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud"
+)
+
+func TestAccSFSTurbo_basic(t *testing.T) {
+	randSuffix := acctest.RandString(5)
+	turboName := fmt.Sprintf("sfs-turbo-acc-%s", randSuffix)
+	resourceName := "sbercloud_sfs_turbo.sfs-turbo1"
+	var turbo shares.Turbo
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSFSTurboDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSFSTurbo_basic(randSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSFSTurboExists(resourceName, &turbo),
+					resource.TestCheckResourceAttr(resourceName, "name", turboName),
+					resource.TestCheckResourceAttr(resourceName, "share_proto", "NFS"),
+					resource.TestCheckResourceAttr(resourceName, "share_type", "STANDARD"),
+					resource.TestCheckResourceAttr(resourceName, "enhanced", "false"),
+					resource.TestCheckResourceAttr(resourceName, "size", "500"),
+					resource.TestCheckResourceAttr(resourceName, "status", "200"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccSFSTurbo_update(randSuffix),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSFSTurboExists(resourceName, &turbo),
+					resource.TestCheckResourceAttr(resourceName, "size", "600"),
+					resource.TestCheckResourceAttr(resourceName, "status", "221"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSFSTurboDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*huaweicloud.Config)
+	sfsClient, err := config.SfsV1Client(SBC_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating Sbercloud sfs turbo client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "sbercloud_sfs_turbo" {
+			continue
+		}
+
+		_, err := shares.Get(sfsClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("SFS Turbo still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckSFSTurboExists(n string, share *shares.Turbo) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*huaweicloud.Config)
+		sfsClient, err := config.SfsV1Client(SBC_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating Sbercloud sfs turbo client: %s", err)
+		}
+
+		found, err := shares.Get(sfsClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("sfs turbo not found")
+		}
+
+		*share = *found
+		return nil
+	}
+}
+
+func testAccNetworkPreConditions(suffix string) string {
+	return fmt.Sprintf(`
+resource "sbercloud_vpc" "test" {
+  name = "tf-acc-vpc-%s"
+  cidr = "192.168.0.0/16"
+}
+
+resource "sbercloud_vpc_subnet" "test" {
+  name       = "tf-acc-subnet-%s"
+  cidr       = "192.168.0.0/16"
+  gateway_ip = "192.168.0.1"
+  vpc_id     = sbercloud_vpc.test.id
+}
+
+resource "sbercloud_networking_secgroup" "secgroup" {
+  name        = "tf-acc-sg-%s"
+  description = "terraform security group for sfs turbo acceptance test"
+}
+`, suffix, suffix, suffix)
+}
+
+func testAccSFSTurbo_basic(suffix string) string {
+	return fmt.Sprintf(`
+%s
+data "sbercloud_availability_zones" "myaz" {}
+
+resource "sbercloud_sfs_turbo" "sfs-turbo1" {
+  name        = "sfs-turbo-acc-%s"
+  size        = 500
+  share_proto = "NFS"
+  vpc_id      = sbercloud_vpc.test.id
+  subnet_id   = sbercloud_vpc_subnet.test.id
+  security_group_id = sbercloud_networking_secgroup.secgroup.id
+  availability_zone = data.sbercloud_availability_zones.myaz.names[0]
+}
+`, testAccNetworkPreConditions(suffix), suffix)
+}
+
+func testAccSFSTurbo_update(suffix string) string {
+	return fmt.Sprintf(`
+%s
+data "sbercloud_availability_zones" "myaz" {}
+
+resource "sbercloud_sfs_turbo" "sfs-turbo1" {
+  name        = "sfs-turbo-acc-%s"
+  size        = 600
+  share_proto = "NFS"
+  vpc_id      = sbercloud_vpc.test.id
+  subnet_id   = sbercloud_vpc_subnet.test.id
+  security_group_id = sbercloud_networking_secgroup.secgroup.id
+  availability_zone = data.sbercloud_availability_zones.myaz.names[0]
+}
+`, testAccNetworkPreConditions(suffix), suffix)
+}
