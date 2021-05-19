f10afb1d0fd94d102b3dbba080cc2034e55ce4bc
Peter Predtechensky <Petr.Predtechensky@gmail.com>
2021-05-19 15:48:21 +0300

    Create main.tf

---
+++ examples/ecs/basic-with-keypair/main.tf
@@ -0,0 +1,44 @@
+# Get the latest Ubuntu image
+data "sbercloud_images_image" "ubuntu_image" {
+  name        = "Ubuntu 20.04 server 64bit"
+  most_recent = true
+}
+
+# Get the list of availability zones
+data "sbercloud_availability_zones" "list_of_az" {}
+
+# Get the flavor name
+data "sbercloud_compute_flavors" "flavor_name" {
+  availability_zone = data.sbercloud_availability_zones.list_of_az.names[0]
+  performance_type  = "normal"
+  cpu_core_count    = 2
+  memory_size       = 4
+}
+
+# Get the subnet where ECS will be created
+data "sbercloud_vpc_subnet" "subnet_01" {
+  name = "place_the_name_of_your_existing_subnet_here"
+}
+
+# Create Key Pair to access frontend nodes
+resource "sbercloud_compute_keypair" "keypair_01" {
+  name       = "terraform-keypair"
+  public_key = "place_your_public_key_here"
+}
+
+# Create ECS
+resource "sbercloud_compute_instance" "ecs_01" {
+  name              = "terraform-ecs"
+  image_id          = data.sbercloud_images_image.ubuntu_image.id
+  flavor_id         = data.sbercloud_compute_flavors.flavor_name.ids[0]
+  security_groups   = ["sg-ssh"]
+  availability_zone = data.sbercloud_availability_zones.list_of_az.names[0]
+  key_pair          = sbercloud_compute_keypair.keypair_01.name
+
+  system_disk_type = "SAS"
+  system_disk_size = 16
+
+  network {
+    uuid = data.sbercloud_vpc_subnet.subnet_01.id
+  }
+}
