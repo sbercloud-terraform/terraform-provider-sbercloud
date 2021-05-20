1e79353098bde44deac9af030aa10bba528c2dfe
Peter Predtechensky <Petr.Predtechensky@gmail.com>
2021-05-20 10:26:54 +0300

    Pass password via variable

--- examples/ecs/basic-with-password/main.tf
+++ examples/ecs/basic-with-password/main.tf
@@ -1,3 +1,9 @@
+# Declare all required input variables
+variable "root_password" {
+  description = "Root password for ECS"
+  sensitive   = true
+}
+
 # Get the latest Ubuntu image
 data "sbercloud_images_image" "ubuntu_image" {
   name        = "Ubuntu 20.04 server 64bit"
@@ -16,7 +22,7 @@ resource "sbercloud_compute_instance" "ecs_01" {
   flavor_id         = "s6.large.2"
   security_groups   = ["sg-ssh"]
   availability_zone = "ru-moscow-1a"
-  admin_pass        = "place_your_root_password_here"
+  admin_pass        = var.root_password
 
   system_disk_type = "SAS"
   system_disk_size = 16
