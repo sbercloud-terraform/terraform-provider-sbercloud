68cc2f67d856fef494143459d35f3d9282d7ca3c
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2021-06-22 06:46:16 +0300

    Add DDS objects (#64)

---
+++ docs/resources/dds_instance.md
@@ -0,0 +1,201 @@
+---
+subcategory: "Document Database Service (DDS)"
+---
+
+# sbercloud\_dds\_instance
+
+Manages dds instance resource within SberCloud
+
+## Example Usage: Creating a Cluster Community Edition
+
+```hcl
+resource "sbercloud_dds_instance" "instance" {
+  name = "dds-instance"
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+
+  availability_zone = "{{ availability_zone }}"
+  vpc_id            = "{{ vpc_id }}"
+  subnet_id         = "{{ subnet_network_id }}}"
+  security_group_id = "{{ security_group_id }}"
+  password          = "Test@123"
+  mode              = "Sharding"
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.c3.medium.4.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.c3.medium.4.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.c3.large.2.config"
+  }
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = "8"
+  }
+}
+```
+
+## Example Usage: Creating a Replica Set
+```hcl
+resource "sbercloud_dds_instance" "instance" {
+  name = "dds-instance"
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+
+  availability_zone = "{{ availability_zone }}"
+  vpc_id            = "{{ vpc_id }}"
+  subnet_id         = "{{ subnet_network_id }}}"
+  security_group_id = "{{ security_group_id }}"
+  password          = "Test@123"
+  mode              = "ReplicaSet"
+  flavor {
+    type      = "replica"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 30
+    spec_code = "dds.mongodb.c3.medium.4.repset"
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region of the DDS instance. Changing this creates
+	a new instance.
+
+* `name` - (Required, String) Specifies the DB instance name. The DB instance name of the same
+	type is unique in the same tenant.
+
+* `datastore` - (Required, List, ForceNew) Specifies database information. The structure is described
+	below. Changing this creates a new instance.
+
+* `availability_zone` - (Required, String, ForceNew) Specifies the ID of the availability zone. Changing
+	this creates a new instance.
+
+* `vpc_id` - (Required, String, ForceNew) Specifies the VPC ID. Changing this creates a new instance.
+
+* `subnet_id` - (Required, String, ForceNew) Specifies the subnet Network ID. Changing this creates a new instance.
+
+* `security_group_id` - (Required, String) Specifies the security group ID of the DDS instance.
+
+* `password` - (Required, String) Specifies the Administrator password of the database instance.
+
+* `disk_encryption_id` - (Required, String, ForceNew) Specifies the disk encryption ID of the instance.
+	Changing this creates a new instance.
+
+* `mode` - (Required, String, ForceNew) Specifies the mode of the database instance. Changing this creates
+	a new instance.
+
+* `flavor` - (Required, List, ForceNew) Specifies the flavors information. The structure is described below.
+	Changing this creates a new instance.
+
+* `backup_strategy` - (Optional, List) Specifies the advanced backup policy. The structure is
+	described below.
+
+* `enterprise_project_id` - (Optional, String, ForceNew) Specifies the enterprise project id of the dds instance.
+  Changing this creates a new instance.
+
+* `ssl` - (Optional, Bool) Specifies whether to enable or disable SSL. Defaults to true.
+
+**Note:** The instance will be restarted in the background when switching SSL. Please operate with caution.
+
+* `tags` - (Optional, Map) The key/value pairs to associate with the DDS instance.
+
+The `datastore` block supports:
+
+* `type` - (Required, String, ForceNew) Specifies the DB engine. 'DDS-Community' and 'DDS-Enhanced' are supported.
+
+* `version` - (Required, String, ForceNew) Specifies the DB instance version. For the Community Edition,
+  the valid values are 3.2, 3.4, or 4.0. For the Enhanced Edition, only 3.4 is supported now.
+
+* `storage_engine` - (Optional, String, ForceNew) Specifies the storage engine of the DB instance.
+  DDS Community Edition supports wiredTiger engine, and the Enhanced Edition supports rocksDB engine.
+
+The `flavor` block supports:
+
+* `type` - (Required, String, ForceNew) Specifies the node type. Valid value:
+  * For a Community Edition cluster instance, the value can be mongos, shard, or config.
+  * For an Enhanced Edition cluster instance, the value is shard.
+  * For a Community Edition replica set instance, the value is replica.
+  * For a Community Edition single node instance, the value is single.
+
+* `num` - (Required, Int, ForceNew) Specifies the node quantity. Valid value:
+	* In a Community Edition cluster instance,the number of mongos ranges from 2 to 16.
+  * In a Community Edition cluster instance,the number of shards ranges from 2 to 16.
+  * In an Enhanced Edition cluster instance, the number of shards ranges from 2 to 12.
+	* config: the value is 1.
+	* replica: the value is 1.
+  * single: The value is 1.
+
+* `storage` - (Optional, String, ForceNew) Specifies the disk type. Valid value: ULTRAHIGH which indicates the type SSD.
+
+* `size` - (Optional, Int, ForceNew) Specifies the disk size. The value must be a multiple of 10. The unit is GB.
+  This parameter is mandatory for nodes except mongos and invalid for mongos.
+
+* `spec_code` - (Required, String, ForceNew) Specifies the resource specification code. In a cluster instance,
+  multiple specifications need to be specified. All specifications must be of the same series,
+  that is, general-purpose (s6), enhanced (c3), or enhanced II (c6). For example:
+  * dds.mongodb.s6.large.4.mongos and dds.mongodb.s6.large.4.config have the same specifications.
+  * dds.mongodb.s6.large.4.mongos and dds.mongodb.c3.large.4.config are not of the same specifications.
+
+The `backup_strategy ` block supports:
+
+* `start_time` - (Required, String) Specifies the backup time window. Automated backups will be triggered
+	during the backup time window. The value cannot be empty. It must be a valid value in the
+	"hh:mm-HH:MM" format. The current time is in the UTC format.
+	* The HH value must be 1 greater than the hh value.
+	* The values from mm and MM must be the same and must be set to any of the following 00, 15, 30, or 45.
+
+* `keep_days` - (Optional, Int) Specifies the number of days to retain the generated backup files. The
+	value range is from 0 to 732.
+	* If this parameter is set to 0, the automated backup policy is not set.
+	* If this parameter is not transferred, the automated backup policy is enabled by default.
+    Backup files are stored for seven days by default.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - Specifies a resource ID in UUID format.
+* `db_username` - Indicates the DB Administator name.
+* `status` - Indicates the the DB instance status.
+* `port` - Indicates the database port number. The port range is 2100 to 9500.
+* `nodes` - Indicates the instance nodes information. Structure is documented below.
+
+The `nodes` block contains:
+
+  - `id` - Indicates the node ID.
+  - `name` - Indicates the node name.
+  - `role` - Indicates the node role.
+  - `type` - Indicates the node type.
+  - `private_ip` - Indicates the private IP address of a node. This parameter is valid only for
+     mongos nodes, replica set instances, and single node instances.
+  - `public_ip` - Indicates the EIP that has been bound on a node. This parameter is valid only for
+     mongos nodes of cluster instances, primary nodes and secondary nodes of replica set instances,
+     and single node instances.
+  - `status` - Indicates the node status.
+
+## Timeouts
+This resource provides the following timeouts configuration options:
+- `create` - Default is 30 minute.
+- `delete` - Default is 30 minute.
+
---
+++ sbercloud/resource_sbercloud_dds_instance_v3_test.go
@@ -0,0 +1,327 @@
+package sbercloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/huaweicloud/golangsdk/openstack/dds/v3/instances"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
+)
+
+func TestAccDDSV3Instance_basic(t *testing.T) {
+	var instance instances.Instance
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "sbercloud_dds_instance.instance"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDDSV3InstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDDSInstanceV3Config_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceName, &instance),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "ssl", "true"),
+					resource.TestCheckResourceAttr(resourceName, "tags.foo", "bar"),
+					resource.TestCheckResourceAttr(resourceName, "tags.owner", "terraform"),
+					resource.TestCheckResourceAttr(resourceName, "backup_strategy.0.start_time", "08:00-09:00"),
+					resource.TestCheckResourceAttr(resourceName, "backup_strategy.0.keep_days", "8"),
+				),
+			},
+			{
+				Config: testAccDDSInstanceV3Config_updateBackupStrategy(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceName, &instance),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "backup_strategy.0.start_time", "00:00-01:00"),
+					resource.TestCheckResourceAttr(resourceName, "backup_strategy.0.keep_days", "7"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDDSV3Instance_withEpsId(t *testing.T) {
+	var instance instances.Instance
+	rName := fmt.Sprintf("tf-acc-test-%s", acctest.RandString(5))
+	resourceName := "sbercloud_dds_instance.instance"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckEpsID(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDDSV3InstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDDSInstanceV3Config_withEpsId(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceName, &instance),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "enterprise_project_id", SBC_ENTERPRISE_PROJECT_ID_TEST),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDDSV3InstanceDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*config.Config)
+	client, err := config.DdsV3Client(SBC_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating SberCloud DDS client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "sbercloud_dds_instance" {
+			continue
+		}
+
+		opts := instances.ListInstanceOpts{
+			Id: rs.Primary.ID,
+		}
+		allPages, err := instances.List(client, &opts).AllPages()
+		if err != nil {
+			return err
+		}
+		instances, err := instances.ExtractInstances(allPages)
+		if err != nil {
+			return err
+		}
+
+		if instances.TotalCount > 0 {
+			return fmt.Errorf("Instance still exists. ")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckDDSV3InstanceExists(n string, instance *instances.Instance) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s. ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set. ")
+		}
+
+		config := testAccProvider.Meta().(*config.Config)
+		client, err := config.DdsV3Client(SBC_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating SberCloud DDS client: %s ", err)
+		}
+
+		opts := instances.ListInstanceOpts{
+			Id: rs.Primary.ID,
+		}
+		allPages, err := instances.List(client, &opts).AllPages()
+		if err != nil {
+			return err
+		}
+		instances, err := instances.ExtractInstances(allPages)
+		if err != nil {
+			return err
+		}
+		if instances.TotalCount == 0 {
+			return fmt.Errorf("dds instance not found.")
+		}
+
+		return nil
+	}
+}
+
+func testAccDDSInstanceV3Config_basic(rName string) string {
+	return fmt.Sprintf(`
+data "sbercloud_availability_zones" "test" {}
+
+data "sbercloud_vpc" "test" {
+  name = "vpc-default"
+}
+
+data "sbercloud_vpc_subnet" "test" {
+  name = "subnet-default"
+}
+
+resource "sbercloud_networking_secgroup" "secgroup_acc" {
+  name = "%s"
+}
+
+resource "sbercloud_dds_instance" "instance" {
+  name              = "%s"
+  availability_zone = data.sbercloud_availability_zones.test.names[0]
+  vpc_id            = data.sbercloud_vpc.test.id
+  subnet_id         = data.sbercloud_vpc_subnet.test.id
+  security_group_id = sbercloud_networking_secgroup.secgroup_acc.id
+  password          = "Test@123"
+  mode              = "Sharding"
+
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.c6.large.2.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 10
+    spec_code = "dds.mongodb.c6.large.2.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.c6.large.2.config"
+  }
+
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = "8"
+  }
+
+  tags = {
+    foo   = "bar"
+    owner = "terraform"
+  }
+}`, rName, rName)
+}
+
+func testAccDDSInstanceV3Config_updateBackupStrategy(rName string) string {
+	return fmt.Sprintf(`
+data "sbercloud_availability_zones" "test" {}
+
+data "sbercloud_vpc" "test" {
+  name = "vpc-default"
+}
+
+data "sbercloud_vpc_subnet" "test" {
+  name = "subnet-default"
+}
+
+resource "sbercloud_networking_secgroup" "secgroup_acc" {
+  name = "%s"
+}
+
+resource "sbercloud_dds_instance" "instance" {
+  name              = "%s"
+  availability_zone = data.sbercloud_availability_zones.test.names[0]
+  vpc_id            = data.sbercloud_vpc.test.id
+  subnet_id         = data.sbercloud_vpc_subnet.test.id
+  security_group_id = sbercloud_networking_secgroup.secgroup_acc.id
+  password          = "Test@123"
+  mode              = "Sharding"
+
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.c6.large.2.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 10
+    spec_code = "dds.mongodb.c6.large.2.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.c6.large.2.config"
+  }
+
+  backup_strategy {
+    start_time = "00:00-01:00"
+    keep_days  = "7"
+  }
+
+  tags = {
+	foo   = "bar"
+    owner = "terraform"
+  }
+}`, rName, rName)
+}
+
+func testAccDDSInstanceV3Config_withEpsId(rName string) string {
+	return fmt.Sprintf(`
+data "sbercloud_availability_zones" "test" {}
+
+data "sbercloud_vpc" "test" {
+  name = "vpc-default"
+}
+
+data "sbercloud_vpc_subnet" "test" {
+  name = "subnet-default"
+}
+
+resource "sbercloud_networking_secgroup" "secgroup_acc" {
+  name = "%s"
+}
+
+resource "sbercloud_dds_instance" "instance" {
+  name                  = "%s"
+  availability_zone     = data.sbercloud_availability_zones.test.names[0]
+  vpc_id                = data.sbercloud_vpc.test.id
+  subnet_id             = data.sbercloud_vpc_subnet.test.id
+  security_group_id     = sbercloud_networking_secgroup.secgroup_acc.id
+  password              = "Test@123"
+  mode                  = "Sharding"
+  enterprise_project_id = "%s"
+
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.c6.large.2.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 10
+    spec_code = "dds.mongodb.c6.large.2.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.c6.large.2.config"
+  }
+
+  backup_strategy {
+    start_time = "00:00-01:00"
+    keep_days  = "7"
+  }
+
+  tags = {
+	foo   = "bar"
+    owner = "terraform"
+  }
+}`, rName, rName, SBC_ENTERPRISE_PROJECT_ID_TEST)
+}
