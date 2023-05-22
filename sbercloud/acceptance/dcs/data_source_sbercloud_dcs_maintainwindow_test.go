def2f8c7e17ce8d59924528d530815825f1fc019
Султанов Артур <aisultanov@sbercloud.ru>
2023-05-22 18:48:21 +0300

    [refactoring]: all dms objects moved to acceptance/dms package

--- sbercloud/acceptance/dcs/data_source_sbercloud_dcs_maintainwindow_test.go
+++ sbercloud/acceptance/dcs/data_source_sbercloud_dcs_maintainwindow_test.go
@@ -1,7 +1,8 @@
-package sbercloud
+package dcs
 
 import (
 	"fmt"
+	"github.com/sbercloud-terraform/terraform-provider-sbercloud/sbercloud/acceptance"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -10,8 +11,8 @@ import (
 
 func TestAccDcsMaintainWindowV1DataSource_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDcsMaintainWindowV1DataSource_basic,
