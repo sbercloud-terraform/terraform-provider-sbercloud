5a6e36281ed73427179b22f2a3cffd3b659dcb3e
Султанов Артур <aisultanov@sbercloud.ru>
2023-05-23 14:20:52 +0300

    [fix]: changed logbnServer url to sbercloud

--- sbercloud/acceptance/swr/resource_sbercloud_swr_organization_test.go
+++ sbercloud/acceptance/swr/resource_sbercloud_swr_organization_test.go
@@ -25,7 +25,7 @@ func TestAccSWROrganization_basic(t *testing.T) {
 	var org namespaces.Namespace
 	rName := acceptance.RandomAccResourceName()
 	resourceName := "sbercloud_swr_organization.test"
-	loginServer := fmt.Sprintf("swr.%s.myhuaweicloud.com", acceptance.SBC_REGION_NAME)
+	loginServer := fmt.Sprintf("swr.%s.sbercloud.ru", acceptance.SBC_REGION_NAME) // "swr.%s.myhuaweicloud.com" было
 
 	rc := acceptance.InitResourceCheck(
 		resourceName,
