bf5700f734254e45a2ee495eda2eb3e6249827db
Ccaswell42 <dmitriykuzin11@yandex.ru>
2023-07-21 14:07:39 +0300

    [add]: test new resource

--- sbercloud/services/dcs2/DCS_CONFIGURATION_PARAMETERS.go
+++ sbercloud/services/dcs2/DCS_CONFIGURATION_PARAMETERS.go
@@ -9,18 +9,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/config"
-	"github.com/sbercloud-terraform/terraform-provider-sbercloud/sbercloud/services/internal/core"
 	"io"
-	"net/http"
-	"strings"
 )
 
-type DCSParams struct {
-	RedisConfig []struct {
-		ParamId    string `json:"param_id"`
-		ParamName  string `json:"param_name"`
-		ParamValue string `json:"param_value"`
-	} `json:"redis_config"`
+type ParamsAttributes struct {
+	ParamId    string `json:"param_id"`
+	ParamName  string `json:"param_name"`
+	ParamValue string `json:"param_value"`
+}
+type ParamsConfig struct {
+	Config []ParamsAttributes `json:"redis_config"`
 }
 
 func ResourceDcsConfigParams() *schema.Resource {
@@ -41,40 +39,17 @@ func ResourceDcsConfigParams() *schema.Resource {
 				ForceNew: true,
 			},
 			"parameters": {
-				Type:     schema.TypeList,
-				Required: true,
+				Type:     schema.TypeMap,
+				Optional: true,
 				ForceNew: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"param_id": {
-							Type:     schema.TypeString,
-							Optional: true,
-							ForceNew: true,
-						},
-						"param_name": {
-							Type:     schema.TypeString,
-							Required: true,
-							ForceNew: true,
-						},
-						"param_value": {
-							Type:     schema.TypeString,
-							Required: true,
-							ForceNew: true,
-						},
-					},
-				},
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 		},
 	}
 }
-func resourceDcsConfigParamsRead3(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-
-	var diags diag.Diagnostics
-	return diags
-}
 
 var (
-	ParamsMap = map[string]string{
+	paramsID = map[string]string{
 		"timeout":                                       "1",
 		"maxmemory-policy":                              "2",
 		"hash-max-ziplist-entries":                      "3",
@@ -101,20 +76,11 @@ var (
 		"client-output-buffer-slave-soft-limit":         "24",
 		"client-output-buffer-slave-hard-limit":         "25",
 		"client-output-buffer-limit-slave-soft-seconds": "26",
-		//:"27",
-		//:"28",
-		//:"29",
-		//:"30",
-		//:"31",
-		//:"32",
-		//:"33",
-		//:"34",
-		"active-expire-num": "35",
+		"active-expire-num":                             "35",
 	}
 )
 
 func resourceDcsConfigParamsCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
 	cfg := meta.(*config.Config)
 	region := cfg.GetRegion(d)
 	client, err := cfg.DcsV2Client(region)
@@ -124,160 +90,28 @@ func resourceDcsConfigParamsCreate(ctx context.Context, d *schema.ResourceData, 
 
 	projectId := d.Get("project_id").(string)
 	instanceId := d.Get("instance_id").(string)
-	parameters := d.Get("parameters").([]interface{})
-	params := DCSParams{}
-	for _, val := range parameters {
-		par := val.(map[string]interface{})
-		paramName := par["param_name"].(string)
-		paramValue := par["param_value"].(string)
-		paramID, ok := ParamsMap[paramName]
+	parameters := d.Get("parameters").(map[string]interface{})
+	paramsConf := ParamsConfig{}
+	for name, value := range parameters {
+		id, ok := paramsID[name]
 		if !ok {
-			return diag.Errorf("No such parameter %s", paramName)
+			return diag.Errorf("No such parameter %s", name)
 		}
-
-		params.RedisConfig = append(params.RedisConfig,
-			struct {
-				ParamId    string `json:"param_id"`
-				ParamName  string `json:"param_name"`
-				ParamValue string `json:"param_value"`
-			}{ParamId: paramID, ParamName: paramName, ParamValue: paramValue})
+		attributes := ParamsAttributes{ParamId: id, ParamName: name, ParamValue: value.(string)}
+		paramsConf.Config = append(paramsConf.Config, attributes)
 	}
 
 	urlString := fmt.Sprintf("https://dcs.ru-moscow-1.hc.sbercloud.ru/v1.0/%s/instances/%s/configs", projectId, instanceId)
 
 	var rst golangsdk.Result
-	resp, err := client.Put(urlString, params, &rst, &golangsdk.RequestOpts{
+	resp, err := client.Put(urlString, paramsConf, &rst, &golangsdk.RequestOpts{
 		OkCodes: []int{204},
 	})
 	if err != nil {
 		return diag.FromErr(err)
 	}
 	d.SetId(resp.Header.Get("X-Request-Id"))
-
-	return diags
-}
-
-func resourceDcsConfigParamsCreate2(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-
-	cfg := meta.(*config.Config)
-
-	var diags diag.Diagnostics
-
-	s := core.Signer{Key: cfg.AccessKey, Secret: cfg.SecretKey}
-
-	projectId := d.Get("project_id").(string)
-	instanceId := d.Get("instance_id").(string)
-	parameters := d.Get("parameters").([]interface{})
-	params := DCSParams{}
-	// попробовать парсить через json.unmarshal()
-	for _, val := range parameters {
-		par := val.(map[string]interface{})
-		paramName := par["param_name"].(string)
-		paramValue := par["param_value"].(string)
-		paramID, ok := ParamsMap[paramName]
-		if !ok {
-			return diag.Errorf("No such parameter %s", paramName)
-		}
-
-		params.RedisConfig = append(params.RedisConfig,
-			struct {
-				ParamId    string `json:"param_id"`
-				ParamName  string `json:"param_name"`
-				ParamValue string `json:"param_value"`
-			}{ParamId: paramID, ParamName: paramName, ParamValue: paramValue})
-	}
-
-	urlString := fmt.Sprintf("https://dcs.ru-moscow-1.hc.sbercloud.ru/v1.0/%s/instances/%s/configs", projectId, instanceId)
-
-	reqBody, err := json.Marshal(params)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	r, err := http.NewRequest("PUT", urlString, strings.NewReader(string(reqBody)))
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	err = s.Sign(r)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	client := http.DefaultClient
-	resp, err := client.Do(r)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	d.SetId(resp.Header.Get("X-Request-Id"))
-	resourceDcsConfigParamsRead(ctx, d, meta)
-	return diags
-}
-
-func resourceDcsConfigParamsRead2(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cfg := meta.(*config.Config)
-
-	var diags diag.Diagnostics
-
-	s := core.Signer{Key: cfg.AccessKey, Secret: cfg.SecretKey}
-
-	projectId := d.Get("project_id").(string)
-	instanceId := d.Get("instance_id").(string)
-	parameters := d.Get("parameters").([]interface{})
-	params := DCSParams{}
-	for _, val := range parameters {
-		par := val.(map[string]interface{})
-		paramName := par["param_name"].(string)
-		paramValue := par["param_value"].(string)
-		paramID, ok := ParamsMap[paramName]
-		if !ok {
-			return diag.Errorf("No such parameter %s", paramName)
-		}
-
-		params.RedisConfig = append(params.RedisConfig,
-			struct {
-				ParamId    string `json:"param_id"`
-				ParamName  string `json:"param_name"`
-				ParamValue string `json:"param_value"`
-			}{ParamId: paramID, ParamName: paramName, ParamValue: paramValue})
-	}
-
-	urlString := fmt.Sprintf("https://dcs.ru-moscow-1.hc.sbercloud.ru/v1.0/%s/instances/%s/configs", projectId, instanceId)
-
-	_, err := json.Marshal(params)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	r, err := http.NewRequest("GET", urlString, strings.NewReader(string("")))
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	err = s.Sign(r)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	client := http.DefaultClient
-	resp, err := client.Do(r)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	respBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-	var respBody DCSParams
-	err = json.Unmarshal(respBytes, &respBody)
-	if err != nil {
-		return diag.Errorf("unmarshall problem: %s", err)
-	}
-	err = d.Set("parameters", findParams(params, respBody))
-	if err != nil {
-		return diag.Errorf("error setting attributes: %s", err)
-	}
-
-	return diags
-
+	return resourceDcsConfigParamsRead(ctx, d, meta)
 }
 
 func resourceDcsConfigParamsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -293,23 +127,15 @@ func resourceDcsConfigParamsRead(ctx context.Context, d *schema.ResourceData, me
 
 	projectId := d.Get("project_id").(string)
 	instanceId := d.Get("instance_id").(string)
-	parameters := d.Get("parameters").([]interface{})
-	params := DCSParams{}
-	for _, val := range parameters {
-		par := val.(map[string]interface{})
-		paramName := par["param_name"].(string)
-		paramValue := par["param_value"].(string)
-		paramID, ok := ParamsMap[paramName]
+	parameters := d.Get("parameters").(map[string]interface{})
+	paramsConf := ParamsConfig{}
+	for name, value := range parameters {
+		id, ok := paramsID[name]
 		if !ok {
-			return diag.Errorf("No such parameter %s", paramName)
+			return diag.Errorf("No such parameter %s", name)
 		}
-
-		params.RedisConfig = append(params.RedisConfig,
-			struct {
-				ParamId    string `json:"param_id"`
-				ParamName  string `json:"param_name"`
-				ParamValue string `json:"param_value"`
-			}{ParamId: paramID, ParamName: paramName, ParamValue: paramValue})
+		attributes := ParamsAttributes{ParamId: id, ParamName: name, ParamValue: value.(string)}
+		paramsConf.Config = append(paramsConf.Config, attributes)
 	}
 
 	urlString := fmt.Sprintf("https://dcs.ru-moscow-1.hc.sbercloud.ru/v1.0/%s/instances/%s/configs", projectId, instanceId)
@@ -328,39 +154,36 @@ func resourceDcsConfigParamsRead(ctx context.Context, d *schema.ResourceData, me
 		return diag.FromErr(err)
 	}
 
-	var respBody DCSParams
+	var respBody ParamsConfig
 	err = json.Unmarshal(respBytes, &respBody)
 	if err != nil {
 		return diag.Errorf("unmarshall problem: %s", err)
 	}
-	err = d.Set("parameters", findParams(params, respBody))
+	err = d.Set("parameters", findParams(paramsConf, respBody))
 	if err != nil {
 		return diag.Errorf("error setting attributes: %s", err)
 	}
 	return diags
 }
 
-func findParams(reqParams, respParams DCSParams) []interface{} {
-	reqParamNames := make([]string, 0)
-	for _, val := range reqParams.RedisConfig {
-		reqParamNames = append(reqParamNames, val.ParamName)
+func resourceDcsConfigParamsDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	d.SetId("")
+
+	return diag.Diagnostics{}
+}
+
+func findParams(reqParams, respParams ParamsConfig) map[string]interface{} {
+	reqParamNames := make([]string, len(respParams.Config))
+	for i := 0; i < len(reqParams.Config); i++ {
+		reqParamNames[i] = reqParams.Config[i].ParamName
 	}
-	res := make([]interface{}, 0)
-	for _, val := range respParams.RedisConfig {
+	res := make(map[string]interface{})
+	for _, val := range respParams.Config {
 		for _, paramName := range reqParamNames {
 			if paramName == val.ParamName {
-				parameters := make(map[string]interface{})
-				parameters["param_id"] = ParamsMap[val.ParamName]
-				parameters["param_value"] = val.ParamValue
-				parameters["param_name"] = val.ParamName
-				res = append(res, parameters)
+				res[paramName] = val.ParamValue
 			}
 		}
 	}
 	return res
-}
-
-func resourceDcsConfigParamsDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	d.SetId("")
-	return diag.Diagnostics{}
 }
