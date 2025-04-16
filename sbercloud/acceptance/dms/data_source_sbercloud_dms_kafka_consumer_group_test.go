28c661ea38607a9cb920347586f0cfd3506f657b
Данила Минашкин <ddminashkin@sberbank.ru>
2025-04-16 16:28:45 +0300

    the key for datasource dms_kafka_consumer_groups has been changed to match the function name

---
+++ sbercloud/acceptance/dms/data_source_sbercloud_dms_kafka_consumer_group_test.go
@@ -0,0 +1,66 @@
+package dms
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sbercloud-terraform/terraform-provider-sbercloud/sbercloud/acceptance"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceDmsKafkaConsumerGroup_basic(t *testing.T) {
+	dataSource := "data.sbercloud_dms_kafka_users.all"
+	rName := acceptance.RandomAccResourceName()
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceDataSourceDmsKafkaConsumerGroup_basic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "users.#"),
+					resource.TestCheckOutput("name_validation", "true"),
+					resource.TestCheckOutput("description_validation", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testDataSourceDataSourceDmsKafkaConsumerGroup_basic(name string) string {
+	return fmt.Sprintf(`
+%s
+
+data "sbercloud_dms_kafka_users" "all" {
+  depends_on = [sbercloud_dms_kafka_consumer_group.test]
+
+  instance_id = sbercloud_dms_kafka_consumer_group.test.instance_id
+}
+
+data "sbercloud_dms_kafka_users" "test" {
+  depends_on = [sbercloud_dms_kafka_user.test]
+
+  instance_id = sbercloud_dms_kafka_instance.test.id
+  name        = sbercloud_dms_kafka_user.test.name
+  description = sbercloud_dms_kafka_user.test.description
+}
+
+locals {
+  test_results = data.sbercloud_dms_kafka_users.test
+}
+
+output "name_validation" {
+  value = alltrue([for v in local.test_results.users[*].name : strcontains(v, sbercloud_dms_kafka_user.test.name)])
+}
+
+output "description_validation" {
+  value = alltrue([for v in local.test_results.users[*].description : strcontains(v, sbercloud_dms_kafka_user.test.description)])
+}
+`, testAccDmsKafkaConsumerGroup_basic(name, "test"))
+}
